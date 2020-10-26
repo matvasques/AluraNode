658e92b2cdb375eb50e968add41293b8a40bcde5
matvasques <matktt@gmail.com>
2020-10-26 10:29:02 -0300

    casaDoCodigo: Continuacao do curso - pt5

--- src/app/routes.js
+++ src/app/routes.js
@@ -22,13 +22,36 @@ module.exports = (app) =>{
     
     app.get('/livros', (req, resp)=>{
         //segundo param da função marko é enviado na variavel data
-        livroDao.lista((erro, resultados) => {
+        livroDao.lista().then(livros =>
             resp.marko(
                 require('./views/livros/lista/lista.marko'),
                 {
-                    livros: resultados
+                    livros: livros
                 }
-            );
-        });
-    });   
+            ))
+            .catch(erro => console.log(erro))
+    });
+
+    //rota para o form
+    app.get('/livros/form', (req, resp) => {
+        resp.marko(require('./views/livros/form/form.marko'))
+    });
+
+    //rota para envio dos dados via metodo post
+    app.post('/livros', (req, resp) => {
+        livroDao.adiciona(req.body).then(resp.redirect('/livros'))
+                                    .catch(erro => console.log(erro));
+    });
+
+    app.get('/busca', (req, resp) => {
+        livroDao.buscaId(req.header).then(livros => {
+            resp.marko(
+                require('./views/livros/busca/busca.marko'),
+                {
+                    livros: livros
+                }
+            )
+        })
+        .catch(erro => console.log(erro))
+    })
 }
